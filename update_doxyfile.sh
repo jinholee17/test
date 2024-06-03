b50e2fc2ad3797882f9e5eb0d6c704b7572bdb24
jinholee17 <jinho_lee@brown.edu>
2024-06-03 14:14:31 -0400

    ALP-108: Change folder setup for Doxygen

--- update_doxyfile.sh
+++ update_doxyfile.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# Get the current branch name
+# This gets name of current branch.
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
-# Set the output directory based on the branch name
+# This sets output directory based on the branch name.
 if [ "$BRANCH" = "main" ]; then
   OUTPUT_DIR="doxy_docs"
 elif [ "$BRANCH" = "dev" ]; then
   OUTPUT_DIR="dev_doxy_docs"
 else
-  OUTPUT_DIR="$BRANCH/doxy_docs"
+  OUTPUT_DIR="local_doxy_docs"
 fi
 
 # Update the OUTPUT_DIRECTORY in the Doxyfile
